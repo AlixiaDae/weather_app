a4ed0f0776dbf1b40899cdc09497655b1c4e018b
AlixiaDae <lizziejones1208@gmail.com>
2023-08-05 23:09:59 +0800

    Refactor for modular pattern
    Now displays forecast for the next three days

--- src/dom.js
+++ src/dom.js
@@ -1,43 +1,183 @@
-import * as api from "./api";
-
-
-
-const dom = (() => {
-    const body = document.body
-    const locationElement = document.querySelector("h2")
-    const imageElement = document.querySelector(".weather-picture")
+function displayTemperature(request, data, scale) {
     const temperatureElement = document.querySelector(".temperature")
-    const messageElement = document.querySelector(".message")
+    let unit = "°C"
 
-    let location = "Milan"
+    if(request === "current") {
+        let tempScale = "celsius" 
+        temperatureElement.textContent = ""
 
-    let url = `http://api.weatherapi.com/v1/forecast.json?key=3e703f9b6f2b40a1ad4122531230308&q=${location.toLowerCase()}`
-    
-    const iconsFolder = "../assets/icons/"
+        if(scale !== tempScale) {
+            unit = "°F"
+            temperatureElement.textContent =`${data.current.temp_f}${unit}`
+        } else {
+            temperatureElement.textContent = `${data.current.temp_c}${unit}`
+        }
+    } else {
+        let tempScale = "celsius"
+        let unit = "°C"
 
-    function getPicture(textCondition) {
-        if(textCondition === "Sunny" || textCondition === "Clear") {
-            imageElement.src = `${iconsFolder}sunny.svg`
-        } else if (textCondition === "Partly cloudy" || textCondition === "Cloudy") {
-            imageElement.src = `${iconsFolder}cloudy.svg`
-        } else if (textCondition === "Patchy rain possible" || textCondition === "Light drizzle" || textCondition === "Patch light rain"|| textCondition === "Light rain" || textCondition === "Moderate rain at times" || textCondition === "Moderate rain" || textCondition === "Light rain shower") {
-            imageElement.src = `${iconsFolder}rainy.svg`
-        } 
+        const forecastTempElements = document.querySelectorAll(".forecast-temp")
+        
+        for(let i = 0; i < forecastTempElements.length; i++ ){
+            let minTempElement = forecastTempElements[i].querySelector(".min-temp")
+            let maxTempElement = forecastTempElements[i].querySelector(".max-temp")
+            let castObject = data.forecast.forecastday[i].day
+
+            if(scale !== tempScale) {
+                unit = "°F"
+                minTempElement.textContent = `${castObject.mintemp_f}${unit}`
+                maxTempElement.textContent = `${castObject.maxtemp_f}${unit}`
+            } else {
+                minTempElement.textContent = `${castObject.mintemp_c}${unit}`
+                maxTempElement.textContent = `${castObject.maxtemp_c}${unit}`
+            }
+        }
+
     }
     
-    async function render(time) {
-        let data = await api.getWeather(url)
-        let processedData = api.getWeatherData(data, time)
+}
 
-        locationElement.textContent = processedData.name
-        messageElement.textContent = processedData.condition
-        temperatureElement.textContent = `${processedData.temperature_c}°C`
-        getPicture(processedData.condition)
+function displayLocation(data) {
+    const locationElement = document.querySelector("h2")
+
+    locationElement.textContent = data.location.name
+}
+
+function getCondition(request, data) {
+    if(request === "current") {
+        const messageElement = document.querySelector(".message")
+
+        messageElement.textContent = `${data.current.condition.text}`
+    } else {
+        const forecastMessageElement = document.querySelectorAll(".forecast-message") 
+        console.log(forecastMessageElement)
+        for(let i = 0; i < forecastMessageElement.length; i++) {
+            let condition = data.forecast.forecastday[i].day.condition.text
+            forecastMessageElement[i].textContent = condition
+        }
     }
+    
+}
 
-    render("current")
+function getPicture(request, data) {
+    const imageElement = document.querySelector(".weather-picture")
+    const imageFolder = '../assets/icons/'
+    const sunny = `${imageFolder}/sunny.svg`
+    const cloudy = `${imageFolder}/cloudy.svg`
+    const lightning = `${imageFolder}/lightning.svg`
+    const rainy = `${imageFolder}/rainy.svg`
+    const snow = `${imageFolder}/snow.svg`
+    const sunnyArray = [
+        "clear",
+        "sunny"
+    ]
+    const rainyArray = [
+        "patchy rain possible",
+        "patchy light drizzle",
+        "light drizzle",
+        "patchy light rain",
+        "light rain",
+        "moderate rain at times",
+        "moderate rain",
+        "heavy rain at times",
+        "heavy rain",
+        "icy pellets",
+        "light rain shower",
+        "moderate or heavy rain shower",
+        "torrential rain shower",
+        "patchy sleet possible",
+        "light sleet",
+        "moderate or heavy sleet",
+        "light sleet showers",
+        "moderate or heavy sleet showers"
+    ]
+    const cloudyArray = [
+        "partly cloudy",
+        "overcast",
+        "cloudy",
+        "mist",
+        "fog"
+    ]
+    const snowArray = [
+        "patchy snow possible",
+        "patchy freezing drizzle possible",
+        "blowing snow",
+        "blizzard",
+        "freezing fog",
+        "freezing drizzle",
+        "heavy freezing drizzle",
+        "light freezing rain",
+        "moderate or heavy freezing rain",
+        "patchy light snow",
+        "light snow",
+        "patchy moderate snow",
+        "moderate snow",
+        "patchy heavy snow",
+        "heavy snow",
+        "light snow showers",
+        "moderate or heavy snow showers",
+        "ice pellets",
+        "light showers of ice pellets",
+        "moderate or heavy showers of ice pellets"
+    ]
+    const lightningArray = [
+        "thundery outbreaks possible",
+        "patchy light rain with thunder",
+        "moderate or heavy rain with thunder",
+        "patchy light snow with thunder",
+        "moderate or heavy snow with thunder"
 
-    return body
-})()
+    ]
+    if(request === "current") {
+    let condition = data.current.condition.text.toLowerCase()
+        if(sunnyArray.includes(condition)) {
+            imageElement.src = sunny
+            imageElement.classList.add("spin")
+        } else if(cloudyArray.includes(condition)) {
+            imageElement.src = cloudy
+            imageElement.classList.remove("spin")
+        } else if(rainyArray.includes(condition)) {
+            imageElement.src = rainy
+            imageElement.classList.remove("spin")
+        } else if(snowArray.includes(condition)) {
+            imageElement.src = snow
+            imageElement.classList.add("spin")
+        } else if(lightningArray.includes(condition)) {
+            imageElement.src = lightning
+            imageElement.classList.remove("spin")
+        }
+    } else {
+        const forecastPictures = document.querySelectorAll(".forecast-picture")
+        for(let i = 0; i < forecastPictures.length; i++) {
+            let forecastCondition = data.forecast.forecastday[i].day.condition.text.toLowerCase()
+            if(sunnyArray.includes(forecastCondition)) {
+                forecastPictures[i].src = sunny
+                forecastPictures[i].classList.add("spin")
+            } else if(cloudyArray.includes(forecastCondition)) {
+                forecastPictures[i].src = cloudy
+                forecastPictures[i].classList.remove("spin")
+            } else if(rainyArray.includes(forecastCondition)) {
+                forecastPictures[i].src = rainy
+                forecastPictures[i].classList.remove("spin")
+            } else if(snowArray.includes(forecastCondition)) {
+                imageElement.src = snow
+                imageElement.classList.add("spin")
+            } else if(lightningArray.includes(forecastCondition)) {
+                forecastPictures[i].src = lightning
+                forecastPictures[i].classList.remove("spin")
+            }
+        }
+        
+    }
+    
+}
 
-export default dom
+
+
+export {
+    displayTemperature,
+    displayLocation,
+    getCondition,
+    getPicture
+}
+    
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import { getForecast,getWeather } from './api'
-import dom from './dom'
-import json from './weather_conditions.json'
+import * as api from './api'
+import * as dom from './dom'
 
-// DOM
-
-const temperatureElement = document.querySelector(".temperature")
-const imageElement = document.querySelector(".weather-picture")
-const message = document.querySelector(".message")
-let locationCode;
-const weatherFolder = "../assets/weather/64x64/day/"
-const iconsFolder = "../assets/icons/"
 let location = "" || "Milan"
+let url = `http://api.weatherapi.com/v1/current.json?key=3e703f9b6f2b40a1ad4122531230308&q=${location.toLowerCase()}`
+let forecastUrl = `http://api.weatherapi.com/v1/forecast.json?key=3e703f9b6f2b40a1ad4122531230308&q=${location.toLowerCase()}&days=3`
+const tempToggle = document.getElementById("temp-toggle")
+let tempScale = "celsius"
 
-dom
+async function getWeatherData(url, tempScale) {
+    let data = await api.getWeather(url)
+    let request = "current"
 
+    dom.displayTemperature(request, data, tempScale)
+    dom.displayLocation(data)
+    dom.getPicture(request, data)
+    dom.getCondition(request, data)
+}
 
+async function getForecastData(forecastUrl, tempScale) {
+    let data = await api.getForecast(forecastUrl)
+    let request = "forecast"
+
+    dom.displayTemperature(request, data, tempScale)
+    dom.getPicture(request, data)
+    dom.getCondition(request, data)
+}
+
+tempToggle.addEventListener("click", () => {
+    if(!tempToggle.checked) {
+        getWeatherData(url, "celsius")
+        getForecastData(forecastUrl, "celsius")
+    } else if(tempToggle.checked) {
+        getWeatherData(url, "fahrenheit")
+        getForecastData(forecastUrl, "fahrenheit")
+    }
+})
+
+getForecastData(forecastUrl, tempScale)
+getWeatherData(url, tempScale)
